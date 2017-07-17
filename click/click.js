b1f9a2549c2a155db71c6f91c50bc7a83768b847
yu <wangtianyu199612@gmail.com>
2017-07-17 20:23:03 +0800

    fix bug

---
+++ click/click.js
@@ -0,0 +1,8 @@
+var text = document.querySelector('.content')
+var confirmButton = document.querySelector('.confirmButton')
+function showMessage() {
+  var blank = document.querySelector('.blank')                                                                     
+  blank.innerText = text.value
+}
+
+confirmButton.addEventListener('click', showMessage, false)
--- click/untitled.js
+++
@@ -1,8 +0,0 @@
-var abc = document.querySelector('.env')
-function showMessage() {
-  var mnb= document.querySelector('.div’）                                                                           
-	mnb.innerText = '.abc'
-}
-
-abc.addEventListener('click', showMessage, false)
-
