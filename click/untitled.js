3c4e525ed934b5fe6de4f0fd55599031ea2acd01
yu <wangtianyu199612@gmail.com>
2017-07-17 13:03:30 +0800

    add click

---
+++ click/untitled.js
@@ -0,0 +1,8 @@
+var abc = document.querySelector('.env')
+function showMessage() {
+  var mnb= document.querySelector('.div’）                                                                           
+	mnb.innerText = '.abc'
+}
+
+abc.addEventListener('click', showMessage, false)
+
