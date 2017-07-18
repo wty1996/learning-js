51973208d3cc340ae7712634452d7c0f36a6113b
yu <wangtianyu199612@gmail.com>
2017-07-18 15:27:26 +0800

    add task4

---
+++ task4/untitled.js
@@ -0,0 +1,9 @@
+var button = document.querySelector('.button')
+function showMessage( ){
+	var text2 = document.querySelector('.text2')
+	var text1 = document.querySelector('.text1')
+	alert  (text1.value)
+	alert  (text2.value)
+}
+
+button.addEventListener('click', showMessage, false)
