56cf14e7a16a81a778c2f659c67bcdabd10c83da
yu <wangtianyu199612@gmail.com>
2017-07-19 23:04:39 +0800

    update

--- task4/changeRed.js
+++ task4/changeRed.js
@@ -1,10 +1,9 @@
-var aaa =document.querySelector('.click2')
-var div =document.querySelectorAll('.div')
+var aaa = document.querySelector('.click2')
+var div = document.querySelectorAll('.div')
 function changeRed() {
-	console.log(1)
 	for (var i = 0; i < div.length; i++) {
 		div[i].style.background = 'red'
 	}
-}	
+}
 
 aaa.addEventListener('click', changeRed, false)
